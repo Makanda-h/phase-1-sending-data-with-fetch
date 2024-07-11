b2178741e131a03f4a8507b22e42e90b7417bb1b
Hope <hopemwaura39@gmail.com>
2024-07-11 12:30:38 +0300

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData (name,email){
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(response => response.json())
+    .then(data =>
+        {const id = data.id;
+            document.body.innerHTML += `<p>${id}</p>`
+        })
+
+    .catch(function (error) {
+    document.body.innerHTML += `<p> ${error.message}</p>`
+  })
+};
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify({
+            name: "Steve",
+            email: "steve@steve.com",
+        }),
+}
